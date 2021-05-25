adf4d8b8c7aa908700d163607dc9f69d4849f224
Francisco <f.goni@usal.edu.ar>
2021-05-25 18:38:12 -0300

    Se pushea el boilerplate general para mods de minecraft.

---
+++ src/main/java/web/app/franciscojosegoni/client/util/ClientUtils.java
@@ -0,0 +1,9 @@
+package web.app.franciscojosegoni.client.util;
+
+import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.api.distmarker.Dist;
+
+@OnlyIn(Dist.CLIENT)
+public class ClientUtils {
+
+}
---
+++ src/main/java/web/app/franciscojosegoni/core/init/ItemInit.java
@@ -0,0 +1,5 @@
+package web.app.franciscojosegoni.core.init;
+
+public class ItemInit {
+
+}
