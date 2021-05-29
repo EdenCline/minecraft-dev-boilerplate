bd85fb754a8aa34c8c8d51cf867f99c87233c59a
EdenCline <franciscojgoni@gmail.com>
2021-05-29 19:18:51 -0300

    Added blocks and Items.

--- src/main/java/web/app/franciscojosegoni/core/init/BlockInit.java
+++ src/main/java/web/app/franciscojosegoni/core/init/BlockInit.java
@@ -1,5 +1,24 @@
 package web.app.franciscojosegoni.core.init;
 
-public class BlockInit {
+import net.minecraft.block.AbstractBlock;
+import net.minecraft.block.Block;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.material.MaterialColor;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraftforge.common.ToolType;
+import net.minecraftforge.fml.RegistryObject;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import web.app.franciscojosegoni.ExampleMod;
 
+public class BlockInit {
+	public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, ExampleMod.MOD_ID);
+	
+	public static final RegistryObject<Block> EXAMPLE_BLOCK = BLOCKS.register("example_block",
+			()-> new Block(AbstractBlock.Properties.of(Material.WOOD, MaterialColor.COLOR_BLUE)
+					.harvestTool(ToolType.AXE).strength(15f, 15f)
+					.harvestLevel(2)
+					.sound(SoundType.ANVIL)));
 }
--- src/main/java/web/app/franciscojosegoni/core/init/ItemInit.java
+++ src/main/java/web/app/franciscojosegoni/core/init/ItemInit.java
@@ -1,5 +1,24 @@
 package web.app.franciscojosegoni.core.init;
 
-public class ItemInit {
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraftforge.fml.RegistryObject;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import web.app.franciscojosegoni.ExampleMod;
 
+
+public class ItemInit {
+	public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, ExampleMod.MOD_ID);
+	
+	public static final RegistryObject<Item> EXAMPLE_ITEM = ITEMS.register("example_item",
+															()-> new Item(
+																	new Item.Properties().tab(ItemGroup.TAB_MISC)));
+	
+	//Block Items
+	public static final RegistryObject<Item> EXAMPLE_BLOCK = ITEMS.register("example_block",
+			()-> new BlockItem(
+					BlockInit.EXAMPLE_BLOCK.get(),
+					new Item.Properties().tab(ItemGroup.TAB_BUILDING_BLOCKS)));
 }
